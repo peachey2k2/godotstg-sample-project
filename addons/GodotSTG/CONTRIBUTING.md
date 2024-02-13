7cd16fed16e449f515586f2b56ccb342ffe430f0
peachey2k2 <ogzpolat@gmail.com>
2024-02-13 15:56:41 +0300

    .

--- addons/GodotSTG/CONTRIBUTING.md
+++
@@ -1,4 +0,0 @@
-wait so you'll write code?
-for me???
-and you'll do it for free??????
-okay go ahead
--- addons/GodotSTG/resources/STGCustomData.cs
+++
@@ -1,9 +0,0 @@
-using Godot;
-
-namespace GodotSTG;
-
-[GlobalClass]
-public partial class STGCustomData:Resource{
-    [Export] public string name {get; set;}
-    [Export] public int health {get; set;}
-}
